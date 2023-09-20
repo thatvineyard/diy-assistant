bdd9d5c013cd8840847ae43f214f643a91172a35
Carl Wingardh <c.a.wingardh@gmail.com>
2023-09-20 09:43:56 +0200

    Added docstring to assitance.py

--- components/assistance.py
+++ components/assistance.py
@@ -4,6 +4,12 @@ CBLUE2   = '\33[94m'
 CEND = '\033[0m'
 
 class Assistance:
+  """
+  An Assistance is a list of actions that can be executed by the assistant.
+  Fill it by running `addAction(callback, description)`.
+    For example: `assistance.addAction(lambda : print("Hello World"), "Print message")`
+  Then execute all the actions sequentially by running `execute()`.
+  """
   
   def __init__(self):
     self.actions: list[tuple[Callable[[], None]], str] = []
