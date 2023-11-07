09cd5e7682f3b70aa524d615bd16d1f2397e07f3
Carl Wingardh <c.a.wingardh@gmail.com>
2023-11-07 12:17:10 +0100

    Moved ssml formatting to it's own utils file

--- components/script.py
+++ components/script.py
@@ -1,4 +1,7 @@
 
+from components.utils.ssmlformatter import SsmlFormatter
+
+
 class Voices:
   ARIA = "en-US-AriaNeural"
   GUY = "en-US-GuyNeural"
@@ -23,65 +26,39 @@ class Styles:
     terrified = "terrified" # 	Expresses a scared tone, with a faster pace and a shakier voice. It sounds like the speaker is in an unsteady and frantic status.
     unfriendly = "unfriendly" # 	Expresses a cold and indifferent tone.
 
+class Line:
+  def __init__(self, text: str, voice: str, style: str = "", styleDegree: int = 1, rate: int = -1):
+    self.text = text
+    self.voice = voice
+    self.style = style
+    self.styleDegree = styleDegree
+    self.rate = rate
+  
+  def toSSML(self):
+    ssml_text = self.text
+    if(self.style != ""):
+        ssml_text = SsmlFormatter.encaseInSpeakingStyleTag(ssml_text, self.style, self.styleDegree)
+    if(self.rate != -1):
+        ssml_text = SsmlFormatter.encaseInRateTag(ssml_text, self.rate)
+    ssml_text = SsmlFormatter.encaseInVoiceTag(ssml_text, self.voice)
+    
+    return ssml_text
+
 class Script:
     def __init__(self, defaultVoice: str = "en-US-AriaNeural"):
-        self.lines = []
+        self.lines: list[Line] = []
         self.defaultVoice = defaultVoice
     
     # Use this function to add a line to the script
     def addLine(self, text: str, voice: str = "", style: str = "", styleDegree: int = 1, rate: int = -1):
-      if(style != ""):
-          text = self.__encaseInSpeakingStyle(text, style, styleDegree)
-      if(rate != -1):
-          text = self.__encaseInRate(text, rate)
-      if(voice == "" or voice == None):
-        voice = self.defaultVoice
-      text = self.__encaseInVoice(text, voice)
-      
-      self.lines.append(text)
+      self.lines.append(Line(text, voice, style, styleDegree, rate))
       
     def getSSML(self):      
-      allLines = "\n".join(self.lines)
-      
-      return self.__encaseInSSML(allLines)
+      allLines = "\n".join(map(lambda line: line.toSSML(), self.lines))
+      allLines = SsmlFormatter.encaseInSSMLTag(allLines)
+      return allLines
 
-    def __encaseInSSML(self, text: str):
-      prefix = '''
-      <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis"
-        xmlns:mstts="https://www.w3.org/2001/mstts" 
-        xml:lang="en-US"
-      >
-      '''
-      postfix = '</speak>'
-      return prefix + text + postfix
-
-    def __encaseInVoice(self, text: str, voice: str):
-      prefix = f'<voice name="{voice}">'
-      postfix = '</voice>'
-      return prefix + text + postfix
-
-    def __encaseInSpeakingStyle(self, text: str, style: str, styleDegree: int = 1):
-        if styleDegree < 0.01:
-          print("Style degree too low, clamping to 0.01")
-          styleDegree = 0.01
-        if styleDegree > 2:
-          print("Style degree too high, clamping to 2")
-          styleDegree = 2
-        
-        prefix = f'<mstts:express-as style="{style}" styledegree="{styleDegree}">'
-        postfix = "</mstts:express-as>"
-
-        return prefix + text + postfix
-  
-    def __encaseInRate(self, text: str, rate: int):
-        if rate < 0.5:
-          print("Rate too low, clamping to 0.5")
-          rate = 0.5
-        if rate > 2:
-          print("Rate too high, clamping to 2")
-          rate = 2
-        
-        prefix = f'<prosody rate="{rate}">'
-        postfix = "</prosody>"
-
-        return prefix + text + postfix
+    def toString(self):
+      result = ""
+      for line in self.lines:
+        result += f'line.voice:  line.text'
